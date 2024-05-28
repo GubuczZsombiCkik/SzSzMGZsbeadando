9d8a07bc517a85ec998b145517b7aad922dc676f
gubucz.zsombor <gubucz.zsombor@ckik.loc>
2024-05-28 14:03:27 +0200

    kepek1

---
+++ main.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration){
+    }
+    counter("count1", 0, 1287, 3000);
+    counter("count2", 100, 5786, 2500);
+    counter("count3", 0, 1440, 3000);
+    counter("count4", 0, 7110, 3000);
+})
+
+// active navbar
+let nav = document.querySelector('.navigation-wrap');
+window.onscroll = function () {
+    if(document.documentElement.scrollTop  > 20){
+        nav.classList.add("scroll-on");
+    }else{
+        nav.classList.remove("scroll-on");
+    }
+}
+
+//nav hide
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
